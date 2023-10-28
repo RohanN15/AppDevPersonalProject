ccd0261690ab98473485b0db7cbedc2f7d3335c7
Rohan Nidumbur <rohan@wireless-10-104-14-152.umd.edu>
2023-10-28 00:02:49 -0400

    Added JavaScript files for both pages. Turns cursor into pointer when user hovers over any button

---
+++ MainPageScript.js
@@ -0,0 +1,1 @@
+document.getElementById("route_finder").style.cursor = "pointer";
---
+++ routeFinderScript.js
@@ -0,0 +1,5 @@
+document.getElementById("enter").style.cursor = "pointer";
+document.getElementById("uber_button").style.cursor = "pointer";
+document.getElementById("Amtrak_button").style.cursor = "pointer";
+document.getElementById("DCmetro_button").style.cursor = "pointer";
+document.getElementById("flightaware_button").style.cursor = "pointer";
