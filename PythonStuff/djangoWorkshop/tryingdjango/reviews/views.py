ca9b3e475cc3fbb67b526d7e3846285e84c2eb1a
Rohan Nidumbur <rohan@wireless-10-104-5-23.umd.edu>
2023-12-04 23:14:33 -0500

    changed from get request to post request oops

--- PythonStuff/djangoWorkshop/tryingdjango/reviews/views.py
+++ PythonStuff/djangoWorkshop/tryingdjango/reviews/views.py
@@ -4,8 +4,10 @@ from rest_framework.response import Response
 # Create your views here.
 reviews = []
 class AppDevClubReviewsView(APIView):
-    def get(self, request):
+    def post(self, request):
         new_rev = request.data.get('review')
         if new_rev:
             reviews.append(new_rev)
+        else:
+            reviews.append("Error: No String found")
         return Response({'reviews': reviews})
