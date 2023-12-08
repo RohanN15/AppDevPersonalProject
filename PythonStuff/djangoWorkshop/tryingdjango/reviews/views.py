694dd952fd81c5c786712849e067f52f2689ca48
Rohan Nidumbur <rohan@wireless-10-104-14-240.umd.edu>
2023-12-07 23:32:46 -0500

    Finished Django HW for the week. Works smoothly

--- PythonStuff/djangoWorkshop/tryingdjango/reviews/views.py
+++ PythonStuff/djangoWorkshop/tryingdjango/reviews/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from reviews.models import Review
+
 # Create your views here.
-reviews = []
 class AppDevClubReviewsView(APIView):
+    def get(self, request):
+        reviews = []
+        names = []
+        emails = []
+        numbers = []
+        for review in Review.objects.filter():
+            reviews.append(review.review_text)
+            names.append(review.name_text)
+            numbers.append(review.number_text)
+            emails.append(review.email_text)
+        return Response({'reviews': reviews, 'names': names, 'numbers': numbers, 'emails': emails})
+
+class CreateAppDevClubReview(APIView):
     def post(self, request):
-        new_rev = request.data.get('review')
-        if new_rev:
-            reviews.append(new_rev)
+        review = request.data['review']
+        name = request.data['name']
+        email = request.data['email']
+        number = request.data['number']
+        if review != '':
+            new_database_entry = Review(review_text=review, name_text=name, email_text=email, number_text=number)
+            new_database_entry.save()
+            return Response({'message': 'success'})
         else:
-            reviews.append("Error: No String found")
-        return Response({'reviews': reviews})
+            return Response({'message': 'failure'})
